c286c8640fb7460e3d785346145bfa45ae075366
iddrader <iddrader59@gmail.com>
2024-03-12 21:16:33 +0300

    Add search functionality

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,33 +1,37 @@
-import { FormEvent, FunctionComponent, useEffect, useRef } from "react";
+import { FormEvent, FormEventHandler, FunctionComponent, useEffect, useRef } from "react";
 import { RootState } from "../../store";
 import { useSelector } from "react-redux";
 import { useAppDispatch } from "../hooks/useAppDispatch";
+import { fetchBooks } from "../../store/action-creators/booksFetched";
 
 const Search: FunctionComponent = () => {
-    const inputRef = useRef<HTMLInputElement>()
+    const inputRef = useRef<HTMLInputElement>(null)
     const books = useSelector((state: RootState) => state.booksFetched)
     const dispatch = useAppDispatch()
 
     useEffect(() => {
-        // dispatch(fetchBooks('the hobbit'))
     }, []);
 
-    const search = (event: FormEvent<HTMLInputElement>) => {
+    const search = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         const title = inputRef.current?.value;
-        console.log(title)
-    }
 
+        if(title != undefined)
+            dispatch(fetchBooks(title)) 
+
+        console.log(books)
+    }
     return (
         <>
-            <form action="">
+            <form action="" onSubmit={search}>
                 <label htmlFor="search">
                     Search
                     <input type="text" name="search" id="search" ref={inputRef}/>
                 </label>
-                <button type="submit" onClick={search}>Search</button>
+                <button type="submit">Search</button>
             </form>
-
+            { books?.loading && <div>Loading</div> }
+            { books?.books.map(book => <div>{book.title}</div>)}
         </>
     );
 }
--- src/store/action-creators/booksFetched.ts
+++ src/store/action-creators/booksFetched.ts
@@ -9,8 +9,8 @@ export const fetchBooks = (request: string) => {
     return async (dispatch: Dispatch<BooksFetchedAction>) => {
         try {
             dispatch({type: BookActionTypes.FETCH_BOOKS})
-            const response = await axios.get(`https://openlibrary.org/search.json?q=${title}`)
-            dispatch({type: BookActionTypes.FETCH_BOOKS_SUCCESS, payload: response.data})
+            const response = await axios.get(`https://openlibrary.org/search.json?q=${title}&limit=10`)
+            dispatch({type: BookActionTypes.FETCH_BOOKS_SUCCESS, payload: response.data.docs})
         } catch (error) {
             dispatch({type: BookActionTypes.FETCH_BOOKS_ERROR, payload: "No books found"})
         }
